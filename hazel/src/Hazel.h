a50881c4007908ec4465b469c1760201d1cc389e
Allan <allan9pires@gmail.com>
2026-06-06 19:46:26 -0300

    Setup basic Application and Entry Point

---
+++ hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// for use by hazel clients
+#include "Hazel/Application.h"
+
+
+// --------EntryPoint----------------
+#include "Hazel/EntryPoint.h"
+// ----------------------------------
