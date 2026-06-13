6cfef2fa6d5561f3474b727661f00c880210553c
Allan <allan9pires@gmail.com>
2026-06-13 17:39:32 -0300

    Implemented event system
    currently the event system is blocking and not buffered

--- hazel/src/Hazel/Log.cpp
+++ hazel/src/Hazel/Log.cpp
@@ -3,11 +3,10 @@
 
 namespace Hazel
 {
-
 	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
 	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
-	void Log::Init()
+	void Log::Init()	
 	{
 		// setting format
 		spdlog::set_pattern("%^[%T] %n: %v%$");
